ed187d356f15e1bfd5d5b9b6c84b82096974c3f2
roberto karlos <robohozza@gmail.com>
2019-08-19 14:10:51 +0200

    update
    update

--- components.js
+++ components.js
@@ -22,6 +22,28 @@ module.exports =
                     data    : {},
                     style   : ''
                 }
+            },
+            'Icon-left' :
+            {
+                title       : 'Button icon left',
+                description : 'Button icon left',
+                template    :
+                {
+                    source  : '<Button label="Test" icon="checkmark-round"></Button>',
+                    data    : {},
+                    style   : ''
+                }
+            },
+            'Icon-Right' :
+            {
+                title       : 'Button icon right',
+                description : 'Button icon right',
+                template    :
+                {
+                    source  : '<Button label="Test" icon="checkmark-round" iconPosition="right"></Button>',
+                    data    : {},
+                    style   : ''
+                }
             }
         }
     }

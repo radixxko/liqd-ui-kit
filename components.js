5c0be515d156227778cc91154852d7b80f010d15
Tomas Korenko <radixxko@gmail.com>
2019-08-19 13:28:57 +0200

    Update components.js

--- components.js
+++ components.js
@@ -1,9 +1,25 @@
 module.exports =
 {
-    'Tlacitko':
+    'Tlacitko' :
     {
         title       : 'Button',
         description : 'Tlacitko',
         template    : '<Tlacitko label="Test"></Tlacitko>',
+        variants    :
+        {
+            'Button-disabled' :
+            {
+                title       : 'Disabled Button',
+                description : 'Disablnute tlacitko',
+                template    :
+                {
+                    source  : '<Tlacitko label="Test" disabled></Tlacitko>',
+                    data    : {},
+                    style   : ':root{ --color: red; }'
+                },
+                data        : {},
+                style       : ':root{ --color: red; }'
+            }
+        }
     }
 }

4c9e3b4121c8f822e02777dee30c48c6632c1263
roberto karlos <robohozza@gmail.com>
2019-08-20 11:08:56 +0200

    daco
    desc

--- components.js
+++ components.js
@@ -40,9 +40,20 @@ module.exports =
                 description : 'Button loader text icon',
                 template    :
                 {
-                    source  : '<Button loading="Loading" onclick="new Promise( r => setTimeout( r, 1000 ))"><i data-icon="start-round" style="padding-right: 4px;"></i>Start</Button>',
+                    source  : '<Button loading="Loa" onclick="new Promise( r => setTimeout( r, 1000 ))"><i data-icon="start-round" style="padding-right: 4px;"></i>Start</Button>',
                     data    : {},
                     style   : ''
+                }
+            },
+            'Button-loader-text-icon' :
+            {
+                title       : 'Button loader text icon',
+                description : 'Button loader text icon',
+                template    :
+                {
+                    source  : '<Button loading="Loa" onclick="new Promise( r => setTimeout( r, 1000 ))"><img style="object-fit: contain;height: 100%;width:auto;" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Available_on_the_App_Store_%28black%29.png/640px-Available_on_the_App_Store_%28black%29.png" /></Button>',
+                    data    : {},
+                    style   : '--form-button-padding:0 0; --form-button-border-width: 0;'
                 }
             }
         }
